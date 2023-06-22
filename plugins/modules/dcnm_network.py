efc3d0cc11624cca8ea20c6616bd174f3a2c6da8
Pari-Dhanani <136296284+Pari-Dhanani@users.noreply.github.com>
2023-06-22 18:32:03 +0530

    Update dcnm_network.py

--- plugins/modules/dcnm_network.py
+++ plugins/modules/dcnm_network.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
-#
 # Copyright (c) 2020-2023 Cisco and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
